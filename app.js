b69472c0164edd0440a3597f360b85f9fdcd31e7
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2025-08-27 01:06:01 +0000

    added script for app

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import router from './routes/index.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// set up ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({extended:true}));
+// routes
+app.use('/', router);
+
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+// ========TEST==========
+// app.get('/check-css', (req, res) => {
+//   const cssPath = path.join(__dirname, 'public/css/style.css');
+//   res.send({ exists: fs.existsSync(cssPath), path: cssPath });
+// });
+
+// ===================
+app.listen(PORT,  (error) => {
+  if(error){
+    throw error;
+  }
+  console.log(`server running on port ${PORT}`)
+});
