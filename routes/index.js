3e60fa4b8bbaa2a2f6f75b1147a79d27b82e922c
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2025-08-27 01:05:06 +0000

    added routes script

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
+router.get(/^\/$|\/index(\.html)?$/, (req, res) => {
+  res.render('index', { title: "Mini Message Board", messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form',  { title: "Add a new message"});
+});
+
+router.get('/messages/:id', (req, res) => {
+  const { id } = req.params;
+  const index = Number(id); 
+  const msg = messages[index]; 
+  res.render('messages', { message: msg });
+});
+
+router.post('/new', (req, res) => {
+  const { user, text } = req.body;
+
+  messages.unshift({
+    text: text,
+    user: user,
+    added: new Date()
+  });
+
+  res.redirect('/');
+});
+
+export default router;
